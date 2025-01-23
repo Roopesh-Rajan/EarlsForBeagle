73860a2f51bc361cc3609f231418c9c41a934597
Roopesh-Rajan <roopeshrajanofficial@gmail.com>
2025-01-23 09:32:07 +0530

    Updated Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,40 +5,14 @@ function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="bg-slate text-white  top-0 left-0 w-full z-10 shadow-md">
+    <header className="bg-slate text-white top-0 left-0 w-full z-10 shadow-md">
       <div className="container mx-auto flex items-center justify-between py-4 px-6 relative">
         <h1 className="text-5xl font-title ml-14 text-gold">Earls</h1>
-        <nav
-          className={`${
-            isMenuOpen ? "flex" : "hidden"
-          } md:flex flex-col md:flex-row md:items-center space-y-4 md:space-y-0 md:space-x-8 md:static absolute top-16 left-0 w-full md:w-auto bg-zinc-900 md:bg-transparent md:shadow-none shadow-lg z-50 transition-all ease-in-out duration-300`}
-        >
-          <a href="#home" className="text-white hover:text-gold px-4 py-2 md:px-0">
-            Home
-          </a>
-          <a href="#about" className="text-white hover:text-gold px-4 py-2 md:px-0">
-            Page
-          </a>
-          <a href="#blog" className="text-white hover:text-gold px-4 py-2 md:px-0">
-            Blog
-          </a>
-          <a href="#portfolio" className="text-white hover:text-gold px-4 py-2 md:px-0">
-            Portfolio
-          </a>
-          <a href="#gallery" className="text-white hover:text-gold px-4 py-2 md:px-0">
-            Shop
-          </a>
-          <a href="#contact" className="text-white hover:text-gold px-4 py-2 md:px-0">
-            Contact
-          </a>
-        </nav>
-        <a href="#contact">
-        <button className="hidden md:inline-block mr-14 bg-slate-500 border border-gold text-white px-6 py-3 hover:bg-[#d9973b] transition">
-          Book a Table
-        </button></a>
+        
+      
         <button
           aria-label="Toggle menu"
-          className="md:hidden text-white focus:outline-none"
+          className="lg:hidden text-white focus:outline-none"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           {isMenuOpen ? (
@@ -47,6 +21,41 @@ function Header() {
             <Menu className="h-6 w-6" aria-label="Open menu" />
           )}
         </button>
+        
+    
+        <nav
+          className={`${
+            isMenuOpen ? "flex" : "hidden"
+          } lg:flex flex-col lg:flex-row lg:items-center space-y-4 lg:space-y-0 lg:space-x-8 lg:static absolute top-16 left-0 w-full lg:w-auto bg-zinc-900 lg:bg-transparent lg:shadow-none shadow-lg z-50 transition-all ease-in-out duration-300`}
+        >
+          <a href="#home" className="text-white hover:text-gold px-4 py-2 lg:px-0">
+            Home
+          </a>
+          <a href="#about" className="text-white hover:text-gold px-4 py-2 lg:px-0">
+            Page
+          </a>
+          <a href="#blog" className="text-white hover:text-gold px-4 py-2 lg:px-0">
+            Blog
+          </a>
+          <a href="#portfolio" className="text-white hover:text-gold px-4 py-2 lg:px-0">
+            Portfolio
+          </a>
+          <a href="#gallery" className="text-white hover:text-gold px-4 py-2 lg:px-0">
+            Shop
+          </a>
+          <a href="#contact" className="text-white hover:text-gold px-4 py-2 lg:px-0">
+            Contact
+          </a>
+        </nav>
+
+     
+        <div className="lg:block hidden">
+          <a href="#contact">
+            <button className="bg-slate-500 border border-gold text-white px-6 py-3 hover:bg-gold transition">
+              Book a Table
+            </button>
+          </a>
+        </div>
       </div>
     </header>
   );
