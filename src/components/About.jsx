0af2dbb173de72ed50394621d1ab3db9fdabd934
Roopesh-Rajan <roopeshrajanofficial@gmail.com>
2025-01-23 10:04:22 +0530

    updated Menusection

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,29 +4,31 @@ import Swoosh from "../assets/swoosh.png";
 function About() {
   return (
     <section id="about" className="py-16 bg-slate text-white">
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center gap-12 px-4">
-        <div className="flex flex-col items-center md:items-start justify-center text-center md:text-left md:w-1/2 md:ml-36">
-          <h4 className="text-4xl font-title text-gold pb-7">About Us</h4>
+      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center gap-8 px-4 md:gap-12">
+        {/* Text Section */}
+        <div className="flex flex-col items-center md:items-start justify-center text-center md:text-left md:w-1/2 md:ml-12">
+          <h4 className="text-4xl font-title text-gold pb-5">About Us</h4>
           <h3 className="text-3xl md:text-5xl font-bold mb-4">
             Heaven and Earth are between
             <span className="text-gold"> each other</span>
           </h3>
-          <img src={Swoosh} alt="Swoosh" className="mb-4 w-16" />
-          <p className="text-gray-300 mb-4">
+          <img src={Swoosh} alt="Swoosh" className="mb-4 w-12 md:w-16" />
+          <p className="text-gray-300 mb-6 leading-relaxed">
             Discover a unique dining experience with the best of both worlds:
-            exquisite meals and a premium atmosphere. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+            exquisite meals and a premium atmosphere. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
           </p>
-          <button className="mt-6 px-6 py-3  border border-gold bg-primary  hover:bg-[#d9973b] text-white  hover:bg-primary-light transition">
+          <button className="mt-6 px-6 py-3 border border-gold bg-primary hover:bg-[#d9973b] text-white transition">
             Learn More
           </button>
         </div>
+        {/* Image Section */}
         <div className="flex justify-center md:w-1/2">
           <img
             src={Chocolate}
--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,61 +1,83 @@
-import FreshProducts from '../assets/fresh-products.png';
-import SkilledChefs from '../assets/skilled-chefs.png';
-import Vegan from '../assets/vegan.png';
-import Dessert from '../assets/dessert.png'; 
-import Steak from'../assets/steak.jpg'
+import FreshProducts from "../assets/fresh-products.png";
+import SkilledChefs from "../assets/skilled-chefs.png";
+import Vegan from "../assets/vegan.png";
+import Dessert from "../assets/dessert.png";
+import Steak from "../assets/steak.jpg";
+
 function MenuSection() {
   return (
-    <section id='portfolio' className="py-16 bg-slate text-white min-h-screen">
-      <div className="container mx-auto grid md:grid-cols-2 gap-0 items-center pl-10">
-        <div className="bg-blueblack p-6 md:p-14 h-full">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10">
+    <section id="portfolio" className="py-10 bg-slate text-white min-h-screen">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center px-4 md:px-10">
+        <div className="bg-blueblack p-6 md:p-10 h-full">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             <div className="flex flex-col items-start">
               <div className="flex items-center justify-center w-16 h-16 bg-gray-800 rounded-full mb-4 hover:scale-110 transform transition duration-300 ease-in-out">
-                <img src={FreshProducts} alt="Icon" className="w-8 h-8" />
+                <img
+                  src={FreshProducts}
+                  alt="Fresh Products"
+                  className="w-8 h-8"
+                />
               </div>
               <h3 className="text-lg font-semibold mb-2">Fresh Products</h3>
               <p className="text-sm text-gray-400 mb-4">
-                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna
-                suscipit, fermentum mattis erat rutrum. Sed suscipit libero lectus.
+                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit
+                amet magna suscipit, fermentum mattis erat rutrum. Sed suscipit
+                libero lectus.
               </p>
               <a href="menu" className="text-primary">
                 More View
               </a>
             </div>
+
             <div className="flex flex-col items-start">
               <div className="flex items-center justify-center w-16 h-16 bg-gray-800 rounded-full mb-4 hover:scale-110 transform transition duration-300 ease-in-out">
-                <img src={SkilledChefs} alt="Icon" className="w-8 h-8" />
+                <img
+                  src={SkilledChefs}
+                  alt="Skilled Chefs"
+                  className="w-8 h-8"
+                />
               </div>
               <h3 className="text-lg font-semibold mb-2">Skilled Chefs</h3>
               <p className="text-sm text-gray-400 mb-4">
-                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna
-                suscipit, fermentum mattis erat rutrum. Sed suscipit libero lectus.
+                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit
+                amet magna suscipit, fermentum mattis erat rutrum. Sed suscipit
+                libero lectus.
               </p>
               <a href="menu" className="text-primary">
                 More View
               </a>
             </div>
+
             <div className="flex flex-col items-start">
               <div className="flex items-center justify-center w-16 h-16 bg-gray-800 rounded-full mb-4 hover:scale-110 transform transition duration-300 ease-in-out">
-                <img src={Vegan} alt="Icon" className="w-8 h-8" />
+                <img
+                  src={Vegan}
+                  alt="Bamboo Bar`s Best Bar"
+                  className="w-8 h-8"
+                />
               </div>
-              <h3 className="text-lg font-semibold mb-2">Bamboo Bar`s Best Bar</h3>
+              <h3 className="text-lg font-semibold mb-2">
+                Bamboo Bar`s Best Bar
+              </h3>
               <p className="text-sm text-gray-400 mb-4">
-                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna
-                suscipit, fermentum mattis erat rutrum. Sed suscipit libero lectus.
+                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit
+                amet magna suscipit, fermentum mattis erat rutrum. Sed suscipit
+                libero lectus.
               </p>
               <a href="menu" className="text-primary">
                 More View
               </a>
             </div>
+
             <div className="flex flex-col items-start">
               <div className="flex items-center justify-center w-16 h-16 bg-gray-800 rounded-full mb-4 hover:scale-110 transform transition duration-300 ease-in-out">
-                <img src={Dessert} alt="Icon" className="w-8 h-8" />
+                <img src={Dessert} alt="Vegan Cuisine" className="w-8 h-8" />
               </div>
               <h3 className="text-lg font-semibold mb-2">Vegan Cuisine</h3>
               <p className="text-sm text-gray-400 mb-4">
-                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna
-                suscipit, fermentum mattis erat rutrum. Sed suscipit libero lectus.
+                Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit
+                amet magna suscipit, fermentum mattis erat rutrum. Sed suscipit
+                libero lectus.
               </p>
               <a href="menu" className="text-primary">
                 More View
@@ -63,11 +85,12 @@ function MenuSection() {
             </div>
           </div>
         </div>
+
         <div className="flex justify-center items-center h-full">
           <img
             src={Steak}
-            alt="steak"
-            className="w-full h-auto md:w-3/4 md:h-auto object-cover"
+            alt="Steak"
+            className="w-full h-auto sm:w-3/4 object-cover rounded-lg shadow-lg"
           />
         </div>
       </div>
@@ -76,4 +99,3 @@ function MenuSection() {
 }
 
 export default MenuSection;
-
